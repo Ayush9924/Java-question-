a7ddbaf65b4ee0eed802d6fcea7fe344d7e0af15
Kumar Ayush <152469050+Ayush9924@users.noreply.github.com>
2026-04-02 21:55:45 +0530

    Add Book.java file

---
+++ Book.java
@@ -0,0 +1,52 @@
+import java.io.Serializable;
+
+public class Book implements Serializable {
+    private int bookId;
+    private String title;
+    private String author;
+    private boolean available;
+
+    public Book(int bookId, String title, String author) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.available = true;
+    }
+
+    public int getBookId() {
+        return bookId;
+    }
+
+    public void setBookId(int bookId) {
+        this.bookId = bookId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{id=" + bookId + ", title='" + title + "', author='" + author + "', available=" + available + "}";
+    }
+}
